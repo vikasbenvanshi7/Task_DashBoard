b60fe5bb0af8a917de536589c63adac793e56bd8
vikas <vikasbenvanshi7@gmail.com>
2022-04-09 11:10:26 +0530

    final release

---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,135 @@
+import Grid from "@mui/material/Grid";
+import MDBox from "components/MDBox";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+import Projects from "layouts/dashboard/components/Projects";
+import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+
+function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
+
+  return (
+    <DashboardLayout>
+      <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="dark"
+                icon="weekend"
+                title="Bookings"
+                count={444}
+                percentage={{
+                  color: "success",
+                  amount: "+55%",
+                  label: "than lask week",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon="leaderboard"
+                title="Today's Users"
+                count="2,30000"
+                percentage={{
+                  color: "success",
+                  amount: "+3%",
+                  label: "than last month",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="store"
+                title="Revenue"
+                count="34kkk"
+                percentage={{
+                  color: "success",
+                  amount: "+1%",
+                  label: "than yesterday",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon="person_add"
+                title="Followers"
+                count="+91111"
+                percentage={{
+                  color: "success",
+                  amount: "",
+                  label: "Just updated",
+                }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+        <MDBox mt={4.5}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsBarChart
+                  color="info"
+                  title="website views"
+                  description="Last Campaign Performance"
+                  date="campaign sent 2 days ago"
+                  chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="success"
+                  title="daily sales"
+                  description={
+                    <>
+                      (<strong>+15%</strong>) increase in today sales.
+                    </>
+                  }
+                  date="updated 4 min ago"
+                  chart={sales}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="completed tasks"
+                  description="Last Campaign Performance"
+                  date="just updated"
+                  chart={tasks}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+        <MDBox>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={8}>
+              <Projects />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <OrdersOverview />
+            </Grid>
+          </Grid>
+        </MDBox>
+      </MDBox>
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
---
+++ src/routes.js
@@ -0,0 +1,72 @@
+
+import Dashboard from "layouts/dashboard";
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Dashboard",
+    key: "dashboard",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/dashboard",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Dummy",
+    key: "Dummy",
+    icon: <Icon fontSize="small">dashboard</Icon>,
+    route: "/#",
+    component: <Dashboard />,
+  },
+];
+
+export default routes;
